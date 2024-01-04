a2cc722e7fd46d807002f85daa168e8e62cccdcd
Ethan Fajnkuchen <102037640+EthanFajnkuchen@users.noreply.github.com>
2024-01-04 13:45:16 +0200

    Best LLM.py -> Communication works with wolfram

--- HelloFromLLM.py
+++ HelloFromLLM.py
@@ -3,5 +3,5 @@ from gpt4all import GPT4All
 # gpt4all-falcon-q4_0.gguf
 # mistral-7b-openorca.Q4_0.gguf
 model = GPT4All("gpt4all-falcon-q4_0.gguf")
-output = model.generate(prompt="The capital of france is")
+output = model.generate(prompt="Who was the first President of the United States?")
 print(output)
